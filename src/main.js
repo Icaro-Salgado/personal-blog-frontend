ca238dadd4c1fab55f183107b10959a9a3f615c7
Icaro G. P. Salgado <icarosalgadogp@gmail.com>
2022-07-10 18:28:42 -0300

    setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createProvider } from './vue-apollo'
 
 createApp(App).use(router).mount('#app')
-
-new Vue({
-    apolloProvider: createProvider(),
-    render: h => h(App)
-}).$mount('#app');
